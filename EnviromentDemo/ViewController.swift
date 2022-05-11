fb8041eb7e36dfaae26aa28375f24a115bd2aa66
Shein <sheinthulwin@gmail.com>
2022-05-12 01:12:52 +0630

    Initial Commit

---
+++ EnviromentDemo/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  EnviromentDemo
+//
+//  Created by ut-i02 on 12/05/2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
