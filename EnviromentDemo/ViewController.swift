5e52d88d355850325690fea77b502e6295d85e0f
Shein <sheinthulwin@gmail.com>
2022-05-12 01:26:15 +0630

    First commit.

--- EnviromentDemo/ViewController.swift
+++ EnviromentDemo/ViewController.swift
@@ -9,9 +9,13 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var lblMessage: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        lblMessage.text = "Enviroment"
     }
 
 
